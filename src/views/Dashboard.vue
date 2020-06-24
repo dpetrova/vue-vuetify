138a87423e60d6919d9984bfd46ab555273cf486
dpetrova <daniella_petrova@abv.bg>
2020-06-24 12:35:28 +0300

    add data table and snackbar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
+  <v-container>
     <h1>Dashboard</h1>
-    <v-data-table
-      :headers="headers"
-      :items="employees"
-      :items-per-page="5"      
-      @click:row="selectRow"
-      class="elevation-1"
-    ></v-data-table>
+    <!-- sales graphs -->
+    <v-row>
+      <v-col v-for="sale in sales" :key="`${sale.title}`">
+        <SalesGraph
+          :sale="sale"
+        ></SalesGraph>
+      </v-col>
+    </v-row>
+     <!-- statistic cards -->
+    <v-row>
+      <v-col v-for="statistic in statistics" :key="`${statistic.title}`">
+        <StatisticCard :statistic="statistic" />
+      </v-col>
+    </v-row>    
+    <!-- employees table and event timeline -->
+    <v-row>
+      <v-col cols="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>        
+    <!-- snackbar notifier -->
     <v-snackbar v-model="snackbar">
-      You have selected {{ currentItem.name}} ({{currentItem.title}})
+      You have selected {{ selectedEmployee.name}} ({{selectedEmployee.title}})
       <v-btn
         color="pink"
         text
@@ -18,36 +35,46 @@
         Close
       </v-btn>
     </v-snackbar>
-  </div>
+  </v-container>
 </template>
 
 <script>
+import EmployeesTable from "../components/EmployeesTable";
+import EventTimeline from "../components/EventTimeline";
+import SalesGraph from "../components/SalesGraph";
+import StatisticCard from "../components/StatisticCard";
+
 import employeesData from "../data/employees.json";
+import timelineData from "../data/timeline.json";
+import salesData from "../data/sales.json";
+import statisticsData from "../data/statistics.json";
 
 export default {
   name: "Dashboard",
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard
+  },
   data() {
     return {
+      employees: employeesData,
+      sales: salesData,
+      statistics: statisticsData,
+      timeline: timelineData,
       snackbar: false,
-      currentItem: "",
-      headers: [
-        {
-          text: "ID",
-          align: "left",
-          sortable: false,
-          value: "id"
-        },
-        { text: "Name", value: "name" },
-        { text: "Title", value: "title" },
-        { text: "Salary", value: "salary" }
-      ],
-      employees: employeesData
+      selectedEmployee: {
+        name: "",
+        title: ""
+      }
     };
   },
   methods: {
-    selectRow(event) {
+    setEmployee(event) {
       this.snackbar = true;
-      this.currentItem = event;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
     }
   }
 };
