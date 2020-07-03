9a602e66c88eb590b221926a6b33c0f0dce286eb
dpetrova <daniella_petrova@abv.bg>
2020-07-03 13:37:09 +0300

    date picker with menu

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,8 +7,28 @@
           <v-text-field label="Email" type="email" />
           <v-autocomplete label="Which browser do you use?" :items="browsers" />
           <v-file-input label="Attach profile picture" />
-          <v-text-field label="Birthday" v-model="birthday" readonly />
-          <v-date-picker v-model="birthday" />
+          <!-- <v-text-field label="Birthday" v-model="birthday" readonly />
+          <v-date-picker v-model="birthday" /> -->
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="birthday"
+                label="Birthday"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+            ></v-text-field>
+            </template>
+            <v-date-picker v-model="birthday" @input="menu = false"></v-date-picker>
+          </v-menu>          
           <v-checkbox label="Agree to terms & conditions" />
           <v-btn type="submit" color="primary">Submit</v-btn>
         </v-form>
@@ -21,6 +41,7 @@
 export default {
   data: () => ({
     browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
+    menu: false,
     birthday: ""
   })
 };
