70f26fe1d264e8e69c7a70d1be0a8691a79a5553
dpetrova <daniella_petrova@abv.bg>
2020-07-03 16:16:37 +0300

    form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,8 +3,13 @@
     <v-row>
       <v-col>
         <h1>Signup Page</h1>
-        <v-form>
-          <v-text-field label="Email" type="email" />
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field 
+            label="Email" 
+            type="email" 
+            v-model="email"
+            :rules="emailRules"
+          />
           <v-autocomplete label="Which browser do you use?" :items="browsers" />
           <v-file-input label="Attach profile picture" />
           <!-- <v-text-field label="Birthday" v-model="birthday" readonly />
@@ -29,8 +34,16 @@
             </template>
             <v-date-picker v-model="birthday" @input="menu = false"></v-date-picker>
           </v-menu>          
-          <v-checkbox label="Agree to terms & conditions" />
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox 
+            label="Agree to terms & conditions" 
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          />          
+          <v-btn type="submit" color="primary" class="mr-4" :disabled="!formValidity">Submit</v-btn>
+          <v-btn color="success" class="mr-4" @click="validateForm">Validate Form</v-btn>
+          <v-btn color="warning" class="mr-4" @click="resetValidation">Reset Validation</v-btn>
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -42,7 +55,40 @@ export default {
   data: () => ({
     browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
     menu: false,
-    birthday: ""
-  })
+    birthday: "",
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      value =>
+        !!value ||
+        "You must agree to the terms and conditions to sign up for an account."
+    ],
+    email: "",
+    emailRules: [
+      value => !!value || "Email is required.",
+      value => value.indexOf("@") !== 0 || "Email should have a username.",
+      value => value.includes("@") || "Email should include an @ symbol.",
+      value =>
+        value.indexOf(".") - value.indexOf("@") > 1 ||
+        "Email should contain a valid domain.",
+      value => value.includes(".") || "Email should include a period symbol.",
+      value =>
+        value.indexOf(".") <= value.length - 3 ||
+        "Email should contain a valid domain extension."
+    ],
+    formValidity: false
+  }),
+  methods: {
+    resetForm() {
+      //reset entire form (input values + validation)
+      this.$refs.signUpForm.reset();
+    },
+    resetValidation() {
+      //reset only form validation
+      this.$refs.signUpForm.resetValidation();
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate();
+    }
+  }
 };
 </script>
